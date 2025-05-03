79c3830b8c95de8e7c87e9875d41f9a8a5d8aafd
Julio Rafael <juliorafaelnft@hotmail.com>
2025-05-03 12:15:37 -0300

    fix main and dev.bat

---
+++ dev.bat
@@ -0,0 +1,2 @@
+@echo off
+py service/main.py
--- service/main.py
+++ service/main.py
@@ -8,4 +8,4 @@ async def read_root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", port=8080, reload=True)
